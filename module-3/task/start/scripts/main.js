67ef2d67ea2d96f88afb6cf3f18354c49f04b614
Basson Katy <Katy.Basson@quornfoods.com>
2021-05-20 21:11:58 +0100

    if else statements added

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -19,3 +19,55 @@ console.log(price);
 console.log(quantity);
 console.log(inStock);
 console.log(discountAmount);
+
+function showProductName() 
+{
+   //code goes here
+   console.log(productName);
+}
+
+showProductName();
+
+function totalPrice(productPrice,productQuantity){
+    console.log(productPrice*productQuantity)
+}
+
+totalPrice(10,5);
+
+//function checkAvailability(productInStock){
+  //  console.log(productInStock);
+//}
+
+// checkAvailability(productInStock){
+// console.log(productInStock);
+// }
+
+// var checkAvailability = (productInStock) => {
+// console.log(productInStock);
+// }
+
+var checkAvailability = productInStock => console.log(productInStock);
+
+checkAvailability(5);
+
+
+var number = 25
+
+
+function checkNumber(number){
+    if (number < 10){
+        return number + ' is less than 10';
+    } else if (number < 20){
+        return number + ' is less than 20 but more than 10';
+    } else if (number < 30){
+        return number + ' is less than 30 and more than 20';
+    } else {
+        return number + ' is not less than 30';
+    }
+}
+
+console.log(checkNumber(8));
+console.log(checkNumber(25));
+console.log(checkNumber(34));
+console.log(checkNumber(14));
+console.log(checkNumber(12));
