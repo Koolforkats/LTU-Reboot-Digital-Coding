923782ebd306902d47c6dd6a2b0a99d5fc7fa508
Basson Katy <Katy.Basson@quornfoods.com>
2021-05-20 19:21:40 +0100

    script changes

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,21 @@
+console.log('Hello');
+
+// Single line comment
+
+/*
+
+multiple line comment
+
+*/
+
+var productName = 'Tinned Beans';
+let price = '0.40';
+const quantity = 1;
+var inStock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
